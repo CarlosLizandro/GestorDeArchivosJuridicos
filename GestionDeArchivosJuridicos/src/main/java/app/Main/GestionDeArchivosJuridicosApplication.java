0a684074cabc1268d59f67154b25afd5126a19ee
CarlosLizandro <creyesp6@upao.edu.pe>
2025-04-13 01:07:42 -0500

    Inicio_Proyecto(Carlos)

---
+++ GestionDeArchivosJuridicos/src/main/java/app/Main/GestionDeArchivosJuridicosApplication.java
@@ -0,0 +1,15 @@
+package app.Main;
+
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+public class GestionDeArchivosJuridicosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionDeArchivosJuridicosApplication.class, args);
+                System.out.println("");
+	}
+
+}
---
+++ GestionDeArchivosJuridicos/src/test/java/app/Main/GestionDeArchivosJuridicosApplicationTests.java
@@ -0,0 +1,13 @@
+package app.Main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionDeArchivosJuridicosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
